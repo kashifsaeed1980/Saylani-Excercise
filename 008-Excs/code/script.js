e9ec30f01d09f67d63d1ab0ca5601d1cf5c81ae6
kashifsaeed1980 <95670611+kashifsaeed1980@users.noreply.github.com>
2023-02-11 22:31:01 +0500

    008-excercise upload JavaScript

---
+++ 008-Excs/code/script.js
@@ -0,0 +1,91 @@
+// let name = "Umair Azmat";
+// let admin = name;
+
+// alert(admin);
+
+// ====================
+
+// let ourPlanet = prompt("What is the name of our planet ?");
+
+// console.log('name of Planet = ' + ourPlanet);
+
+// ======================
+
+let currentVisitor = 'Amin';
+
+// ======================
+
+let favDrink = 'Coke';
+
+// ======================
+
+let favNumber = 5;
+
+// ======================
+
+function myFav(a,b) {
+    console.log(a + b);
+}
+
+myFav(2, 5);
+
+// ======================
+
+let persion = {
+    name: 'Umair',
+    age: 20
+}
+// ======================
+
+let a = 24;
+let b = 15;
+let c = a + b;
+
+console.log("result of addition is " +c);
+// =====================
+
+let x = 10;
+let y = 5;
+let z = x / y;
+
+console.log("result of firstProgram ", z);
+// =====================
+
+let m = 36;
+let n = 4;
+let o = (m + n)/5;
+
+console.log("result of secondProgram ", o);
+// =====================
+
+let far = 50; 
+let Cel = (far - 32) * 5 / 9;
+
+console.log("temp is " + Cel + " C");
+// =====================
+
+// let firstNumber = Number(prompt('Please write your first favourite number between 1 to 10'));
+// let secondNumber = Number(prompt('Please write your second favourite number between 1 to 10'));
+// let addition = firstNumber + secondNumber;
+
+// alert('sum of your favourite number is ' + addition);
+
+// =====================
+
+// let myFirstNumber = Number(prompt('Please write your first favourite number between 10 to 20'));
+// let mySecondNumber = Number(prompt('Please write your Second favourite number between 1 to 5'));
+
+// let division = myFirstNumber / mySecondNumber;
+
+// console.log('quotient of two numbers is ' + division);
+
+// ======================
+
+let myFirstNumber = Number(prompt('Please write your first favourite number between 10 to 20'));
+let mySecondNumber = Number(prompt('Please write your Second favourite number between 1 to 5'));
+
+let reminder = myFirstNumber % mySecondNumber;
+
+console.log(myFirstNumber + " % " + mySecondNumber + " = " + reminder );
+
+
