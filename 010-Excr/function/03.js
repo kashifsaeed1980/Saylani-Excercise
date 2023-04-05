055789230b45971a56008f556d91e1e5ad94dc96
Kashif Saeed <95670611+kashifsaeed1980@users.noreply.github.com>
2023-04-05 20:58:41 +0500

    delete function directory of Ahmad

--- 010-Excr/function/03.js
+++
@@ -1,8 +0,0 @@
-// 3. Write a js program to find maximum and minimum between two numbers using functions.
-
-function comp(a, b) {
-  console.log("Max Number = " + Math.max(a, b));
-  console.log("Min Number = " + Math.min(a, b));
-}
-
-comp(135, 50);
--- 010-Excr/function/15.js
+++
@@ -1,8 +0,0 @@
-// 15. Write a js program to find reverse of any number using function.
-
-function anyNum(n) {
-  for (let i = n; i > 0; i--) {
-    console.log(i);
-  }
-}
-anyNum(15);
